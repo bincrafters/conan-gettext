c3ea3770d4b43ec3d0c8136be4725e6728fc1b2f
SSE4 <tomskside@gmail.com>
2019-07-11 16:23:36 +0200

    - fix OSX build
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -10,7 +10,7 @@ int main(int argc, char * const argv[])
 	bindtextdomain("conan", argv[1]);
 	textdomain("conan");
 	setlocale(LC_ALL, "");
-	char * lang = std::getenv("LANG");
+	const char * lang = std::getenv("LANG");
 	lang = lang ? lang : "";
 	std::cout << "hello in " << lang << ": " << gettext("hello") << std::endl;
     return 0;
